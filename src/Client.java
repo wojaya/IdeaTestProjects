6b14b6af38957616b1a7e1a0b8595e5d4fb05f2a
wojaya <wojaya@outlook.com>
2017-11-15 11:57:34 +0800

    learn java happily -,-

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,33 @@
 public class Client {
 
     public static void main(String[] args) {
-        TT tt = new TT();
-        Gen<TT> iob = new Gen<>(tt);
-        iob.showType();
-        iob.getOb().showTestBound();
-        iob.getOb().showGenInterface();
+
+
+        TestBound tb = new TestBound();
+        TestBound1 tb1 = new TestBound1();
+        TestBound2 tb2 = new TestBound2();
+        TestBound3 tb3 = new TestBound3();
+        TestBound4 tb4 = new TestBound4();
+        TestBound5 tb5 = new TestBound5();
+
+
+        Gen<TestBound> ob1 = new Gen<>(tb);
+        ob1.showType();
+        Gen<TestBound2> ob2 = new Gen<>(tb2);
+        ob2.showType();
+        Gen<TestBound3> ob3 = new Gen<>(tb3);
+        ob3.showType();
+        Gen<TestBound4> ob4 = new Gen<>(tb4);
+        ob4.showType();
+        Gen<TestBound5> ob5 = new Gen<>(tb5);
+        ob5.showType();
+
+        ob1.isSameFreq(ob1);
 
     }
 }
 
-class Gen<T extends TestBound & GenInterface> {
+class Gen<T extends TestBound> {
     T ob;
 
     Gen(T o) {
@@ -22,7 +39,17 @@ class Gen<T extends TestBound & GenInterface> {
     }
 
     void showType() {
-        System.out.println("Type of T is: " + ob.getClass().getName());
+        System.out.println("Type of this is: " + ob.getClass().getName());
+    }
+
+    int getFreq() {
+        return 1;
+    }
+
+    void isSameFreq(Gen<? super TestBound3> ob) {
+        if (getFreq() == ob.getFreq()) {
+            System.out.println("YEsYEsYEs");
+        }
     }
 }
 
@@ -32,7 +59,37 @@ interface GenInterface {
 
 class TestBound {
     public void showTestBound() {
-        System.out.println("show Test Bound");
+        System.out.println("This is show Test Bound");
+    }
+}
+
+class TestBound1 extends TestBound {
+    public void showTestBound() {
+        System.out.println("This is show Test Bound111");
+    }
+}
+
+class TestBound2 extends TestBound1 {
+    public void showTestBound() {
+        System.out.println("This is show Test Bound222");
+    }
+}
+
+class TestBound3 extends TestBound2 {
+    public void showTestBound() {
+        System.out.println("This is show Test Bound333");
+    }
+}
+
+class TestBound4 extends TestBound3 {
+    public void showTestBound() {
+        System.out.println("This is show Test Bound444");
+    }
+}
+
+class TestBound5 extends TestBound4 {
+    public void showTestBound() {
+        System.out.println("This is show Test Bound555");
     }
 }
 
